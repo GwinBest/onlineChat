6c070dc91de5ecd87daf7272dc538444b0b68d29
Matvey <thegwinbest@gmail.com>
2024-11-23 10:49:21 +0300

    refactoring

--- src/gui/chat/chatPage.cpp
+++ src/gui/chat/chatPage.cpp
@@ -19,29 +19,6 @@ namespace Gui
         _ui->availableChatsList->setResizeMode(QListView::Adjust);
 
         connect(_ui->searchInput, &QLineEdit::textChanged, this, &ChatPage::OnSearchInputTextChanged);
-        //_ui->avaliableChatsList->setModel()
-        //for (int i = 0; i < 10; ++i) { // 10 элементов
-        //    QString userName = QString("User %1").arg(i + 1);
-
-        //    // Создаем кастомный виджет
-        //    Widgets::ChatUserWidget* customWidget = new Widgets::ChatUserWidget(userName.toStdString());
-
-        //    // Создаем элемент списка
-        //    QListWidgetItem* item = new QListWidgetItem(_ui->listWidget);
-        //    item->setSizeHint(QSize(270, 80)); // Устанавливаем размер элемента
-
-        //    // Добавляем виджет в элемент списка
-        //    _ui->listWidget->addItem(item);
-        //    _ui->listWidget->setItemWidget(item, customWidget);
-        //}
-
-        //// Настройка QListWidget
-        //_ui->listWidget->setSpacing(0);
-        //_ui->listWidget->setSelectionMode(QAbstractItemView::SingleSelection);
-        //_ui->listWidget->setStyleSheet(
-        //    "QListWidget::item :hover { background-color: #d9d9d9; }"
-        //);
-
     }
 
     ChatPage::~ChatPage()
@@ -70,9 +47,5 @@ namespace Gui
         }
 
         _model->SetMatchingChats(userSearch.toStdString());
-
-
-
-
     }
 } // !namespace Gui

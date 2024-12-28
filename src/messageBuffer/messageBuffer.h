ea83c694cb5b7eacbc960c46d9b302e3dc3597de
Matvey <thegwinbest@gmail.com>
2024-12-28 21:46:39 +0300

    refactoring

--- src/messageBuffer/messageBuffer.h
+++ src/messageBuffer/messageBuffer.h
@@ -17,7 +17,7 @@ namespace MessageBuffer
         MessageNode(const MessageStatus messageType, std::string data, std::string sendTime)
             : data(std::move(data))
             , sendTime(std::move(sendTime))
-            , messageType(messageType)
+            , messageType(std::move(messageType))
         {}
 
         std::string data;

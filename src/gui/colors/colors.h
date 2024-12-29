9fe46a6a0c998bbc6a1ed783474b29939167626d
Matvey <thegwinbest@gmail.com>
2024-12-29 13:23:27 +0300

    added static keyword

--- src/gui/colors/colors.h
+++ src/gui/colors/colors.h
@@ -4,16 +4,16 @@
 
 namespace Gui
 {
-    constexpr const char* const colorWhite = "#FFFFFF";
-    constexpr const char* const colorRed = "#FF0000";
-    constexpr const char* const colorLightGreen = "#00FF84";
-    constexpr const char* const colorBlue = "#697684";
-    constexpr const char* const colorGray = "#282e33";
-    constexpr const char* const colorOnHover = "#505050";
-    constexpr const char* const colorSelectedChat = "#009687";
-    constexpr const char* const colorTransparent = "#00000000";
-    constexpr const char* const colorMessageSend = "#2a2f33";
-    constexpr const char* const colorMessageReceived = "#33393f";
-    constexpr const char* const colorDateDivider = "#2f343b";
-    constexpr const char* const colorMessageTimeSend = "#808080";
+    static constexpr const char* const colorWhite = "#FFFFFF";
+    static constexpr const char* const colorRed = "#FF0000";
+    static constexpr const char* const colorLightGreen = "#00FF84";
+    static constexpr const char* const colorBlue = "#697684";
+    static constexpr const char* const colorGray = "#282e33";
+    static constexpr const char* const colorOnHover = "#505050";
+    static constexpr const char* const colorSelectedChat = "#009687";
+    static constexpr const char* const colorTransparent = "#00000000";
+    static constexpr const char* const colorMessageSend = "#2a2f33";
+    static constexpr const char* const colorMessageReceived = "#33393f";
+    static constexpr const char* const colorDateDivider = "#2f343b";
+    static constexpr const char* const colorMessageTimeSend = "#808080";
 } // !namespace Gui

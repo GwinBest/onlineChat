f15e974a3badf3502d0e6a1ec9e44dbfc6cc20ef
Matvey <thegwinbest@gmail.com>
2024-12-29 13:22:41 +0300

    removed hard coded colors

--- src/gui/chat/scrollArea/chatScrollArea.cpp
+++ src/gui/chat/scrollArea/chatScrollArea.cpp
@@ -6,6 +6,8 @@
 #include <QSpacerItem>
 #include <QVBoxLayout>
 
+#include "gui/colors/colors.h"
+
 namespace Gui::ScrollArea
 {
     QWidget* CreateMessage(const MessageBuffer::MessageNode& message)
@@ -13,20 +15,22 @@ namespace Gui::ScrollArea
         auto* messageWidget = new QWidget();
         auto* layout = new QHBoxLayout(messageWidget);
 
-        const QDateTime dateTime = QDateTime::fromString(message.sendTime.c_str(), "yyyy-MM-dd HH:mm:ss");
+        const QDateTime dateTime = QDateTime::fromString(message.sendTime.c_str(),
+                                                         "yyyy-MM-dd HH:mm:ss");
         const QString sendTime = dateTime.time().toString("HH:mm");
 
         const auto content = QString(
             "<div style='font-size: 14px;'>%1</div>"
-            "<div style='color: gray; font-size: 10px; text-align: right;'>%2</div>")
+            "<div style='color: " + QString(colorMessageTimeSend) +
+            ";font-size: 10px; text-align: right;'>%2</div>")
             .arg(message.data.c_str())
             .arg(sendTime);
 
         auto* messageLabel = new QLabel(content);
         messageLabel->setWordWrap(true);
         messageLabel->setStyleSheet(message.messageType == MessageBuffer::MessageStatus::kSend
-                                    ? "background-color: #2a2f33; border-radius: 10px; padding: 8px;"
-                                    : "background-color: #33393f; border-radius: 10px; padding: 8px;");
+                                    ? "background-color: " + QString(colorMessageSend) + ";border-radius: 10px; padding: 8px;"
+                                    : "background-color: " + QString(colorMessageReceived) + ";border-radius: 10px; padding: 8px;");
 
         if (message.messageType == MessageBuffer::MessageStatus::kSend)
         {
@@ -51,14 +55,14 @@ namespace Gui::ScrollArea
         QFrame* leftLine = new QFrame();
         leftLine->setFrameShape(QFrame::HLine);
         leftLine->setFrameShadow(QFrame::Sunken);
-        leftLine->setStyleSheet("color: gray;");
+        leftLine->setStyleSheet("color: " + QString(colorDateDivider) + ';');
 
         QLabel* dateLabel = new QLabel(dateText);
         dateLabel->setAlignment(Qt::AlignCenter);
         dateLabel->setStyleSheet(
-            "color: white;"
-            "background-color: #2f343b;"
-            "border-radius: 12px;"
+            "color: " + QString(colorWhite) +
+            ";background-color: " + QString(colorDateDivider) +
+            ";border-radius: 12px;"
             "padding: 4px 12px;"
             "font-size: 12px;"
             "font-weight: bold;"
@@ -67,7 +71,7 @@ namespace Gui::ScrollArea
         QFrame* rightLine = new QFrame();
         rightLine->setFrameShape(QFrame::HLine);
         rightLine->setFrameShadow(QFrame::Sunken);
-        rightLine->setStyleSheet("color: gray;");
+        rightLine->setStyleSheet("color: " + QString(colorDateDivider) + ';');
 
         layout->addWidget(leftLine);
         layout->addWidget(dateLabel);
